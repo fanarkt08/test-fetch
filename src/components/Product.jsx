fe7840da1665b2a5f3ef5c42c989b233b1a5e70a
fanarkt08 <fana@n-vibe.fr>
2025-06-23 13:51:02 +0200

    reduce

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,25 +4,22 @@ import '../assets/Product.scss';
 function Product({ product }) {
   return (
     <Card className="mb-3 h-100 mx-2">
-      <div className="product-image-wrapper">
-        <Card.Img
-          variant="top"
-          src={product.image}
-          alt={product.title}
-          className="product-image"
-        />
-      </div>
-        <Card.Body>
-            <Card.Title>{
-                product.title}
-            </Card.Title>
-            <Card.Text>
-                {product.description.substring(0, 150)}...
-            </Card.Text>
-            <Card.Text>
-                {product.price} €
-            </Card.Text>
-        </Card.Body>
+      <Card.Img
+        variant="top"
+        src={product.image}
+        alt={product.title}
+      />
+      <Card.Body>
+          <Card.Title>{
+              product.title}
+          </Card.Title>
+          <Card.Text>
+              {product.description}
+          </Card.Text>
+          <Card.Text>
+              {product.price} €
+          </Card.Text>
+      </Card.Body>
     </Card>
   );
 }
